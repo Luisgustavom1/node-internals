65dd05fe72f726a0ca30ce4d8e40232131056674
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-11-16 08:45:31 -0300

    refactor debug

--- async_hooks/debug.js
+++ async_hooks/debug.js
@@ -1,8 +1,9 @@
 const fs = require("node:fs");
 const util = require('node:util');
+const path = require("node:path");
 
 function debug(...args) {
-  fs.writeFileSync('log.out', `${util.format(...args)}\n`, { flag: 'a' });
+  fs.writeFileSync(`${path.basename(require.main.filename, ".js")}.log.out`, `${util.format(...args)}\n`, { flag: 'a' });
 }
 
 module.exports = { debug } 
